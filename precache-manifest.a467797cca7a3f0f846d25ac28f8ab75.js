9e43d52a6dc6cd3d8fd0534786ce807eb5e55a6e
dayongbz <dayongbz@gmail.com>
2019-08-07 20:39:15 +0900

    Updates

---
+++ precache-manifest.a467797cca7a3f0f846d25ac28f8ab75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "599235b0921280cb94beaacc44e9239c",
+    "url": "/ebs-kids/index.html"
+  },
+  {
+    "revision": "36589e732f16b874ca08",
+    "url": "/ebs-kids/static/css/main.d79b0621.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/ebs-kids/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "36589e732f16b874ca08",
+    "url": "/ebs-kids/static/js/main.9a9346c9.chunk.js"
+  },
+  {
+    "revision": "959c9e23e684bbdb9bb5",
+    "url": "/ebs-kids/static/js/runtime~main.08477fd0.js"
+  }
+]);
