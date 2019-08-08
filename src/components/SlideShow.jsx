edff310814151d629fe4b81cc8869d9372946d04
dayongbz <dayongbz@gmail.com>
2019-08-08 16:10:20 +0900

    슬라이드 기능 완성중

--- src/components/SlideShow.jsx
+++ src/components/SlideShow.jsx
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 export default function SlideShow() {
+  const slideCountRef = useRef(0);
+  const slideRef = useRef();
+  const slideListRef = useRef();
+  useEffect(() => {
+    slideCountRef.current = 1;
+    setInterval(() => {
+      if (slideCountRef.current === 5) {
+        slideCountRef.current = 0;
+      }
+      if (slideCountRef.current === 0) {
+        slideListRef.current.children[4].classList.remove('running');
+        slideListRef.current.children[slideCountRef.current].classList.add(
+          'running',
+        );
+      } else {
+        slideListRef.current.children[slideCountRef.current].classList.add(
+          'running',
+        );
+        slideListRef.current.children[
+          slideCountRef.current - 1
+        ].classList.remove('running');
+      }
+      slideRef.current.style.backgroundImage = `url(images/slide${++slideCountRef.current}.jpg)`;
+    }, 5500);
+  }, []);
   return (
     <div id="slideShowWrap">
       <div
         id="slideShow"
         style={{
-          backgroundImage:
-            'url(images/f16bf41c-bd0d-472c-9dd8-a349472dac542.png)',
+          backgroundImage: 'url(images/slide1.jpg)',
         }}
+        ref={slideRef}
       />
       <div
         style={{
@@ -21,26 +46,31 @@ export default function SlideShow() {
           bottom: '0',
         }}
       />
-      <ul>
-        <li>
+      <ul ref={slideListRef}>
+        <li className="running">
+          <div className="cover" />
           <p className="sub">최근 인기 프로그램</p>
-          <p className="programName">미니 특공대</p>
+          <p className="programName">최고다 호기심딱지</p>
         </li>
         <li>
+          <div className="cover" />
           <p className="sub">최근 인기 프로그램</p>
-          <p className="programName">미니 특공대</p>
+          <p className="programName">출동 슈펑윙스</p>
         </li>
         <li>
+          <div className="cover" />
           <p className="sub">최근 인기 프로그램</p>
-          <p className="programName">미니 특공대</p>
+          <p className="programName">생방송 판다다</p>
         </li>
         <li>
+          <div className="cover" />
           <p className="sub">최근 인기 프로그램</p>
-          <p className="programName">미니 특공대</p>
+          <p className="programName">딩동댕 유치원</p>
         </li>
         <li>
+          <div className="cover" />
           <p className="sub">최근 인기 프로그램</p>
-          <p className="programName">미니 특공대</p>
+          <p className="programName">뽀로로와 노래해요</p>
         </li>
       </ul>
     </div>
