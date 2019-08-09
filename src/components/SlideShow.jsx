40f639ef88dfda31a98b4ef8b62b4f2a125ec87e
dayongbz <dayongbz@gmail.com>
2019-08-09 12:18:11 +0900

    슬라이드 기능 완성

--- src/components/SlideShow.jsx
+++ src/components/SlideShow.jsx
@@ -1,40 +1,104 @@
 import React, { useState, useEffect, useRef } from 'react';
 
 export default function SlideShow() {
-  const slideCountRef = useRef(0);
+  const [slideCount, setSlideCount] = useState(0);
+  const [slideState, setSlideState] = useState(true);
+  const slideIntRef = useRef();
   const slideRef = useRef();
+  const slideWrapRef = useRef();
   const slideListRef = useRef();
+  const preloadRef = useRef();
+
+  function mouseEnter() {
+    setSlideState(false);
+    slideListRef.current.children[slideCount - 1].classList.remove('running');
+  }
+
+  function mouseLeave() {
+    setSlideState(true);
+    slideListRef.current.children[slideCount - 1].classList.add('running');
+  }
+
+  function mouseClick(event) {
+    setSlideCount(+event.currentTarget.dataset.number);
+    slideRef.current.style.backgroundImage = `url(images/slide${+event
+      .currentTarget.dataset.number}.jpg)`;
+  }
+
   useEffect(() => {
-    slideCountRef.current = 1;
-    setInterval(() => {
-      if (slideCountRef.current === 5) {
-        slideCountRef.current = 0;
-      }
-      if (slideCountRef.current === 0) {
-        slideListRef.current.children[4].classList.remove('running');
-        slideListRef.current.children[slideCountRef.current].classList.add(
-          'running',
+    slideWrapRef.current.addEventListener('mouseenter', mouseEnter);
+    slideWrapRef.current.addEventListener('mouseleave', mouseLeave);
+    for (let i = 0; i < slideListRef.current.children.length; i++) {
+      slideListRef.current.children[i].addEventListener('click', mouseClick);
+    }
+    if (slideState) {
+      slideIntRef.current = setInterval(() => {
+        if (slideCount === slideListRef.current.children.length) {
+          setSlideCount(1);
+          slideListRef.current.children[
+            slideListRef.current.children.length - 1
+          ].classList.remove('running');
+          slideListRef.current.children[0].classList.add('running');
+          slideRef.current.style.backgroundImage = `url(images/slide1.jpg)`;
+        } else {
+          if (slideCount === 0) {
+            slideListRef.current.children[
+              slideListRef.current.children.length - 1
+            ].classList.remove('running');
+            slideListRef.current.children[slideCount].classList.add('running');
+          } else {
+            slideListRef.current.children[slideCount].classList.add('running');
+            slideListRef.current.children[slideCount - 1].classList.remove(
+              'running',
+            );
+          }
+          slideRef.current.style.backgroundImage = `url(images/slide${slideCount +
+            1}.jpg)`;
+          setSlideCount(slideCount + 1);
+        }
+      }, 5500);
+    }
+
+    return () => {
+      clearInterval(slideIntRef.current);
+      slideWrapRef.current.removeEventListener('mouseenter', mouseEnter);
+      slideWrapRef.current.removeEventListener('mouseleave', mouseLeave);
+      for (let i = 0; i < slideListRef.current.children.length; i++) {
+        slideListRef.current.children[i].removeEventListener(
+          'click',
+          mouseClick,
         );
-      } else {
-        slideListRef.current.children[slideCountRef.current].classList.add(
-          'running',
-        );
-        slideListRef.current.children[
-          slideCountRef.current - 1
-        ].classList.remove('running');
       }
-      slideRef.current.style.backgroundImage = `url(images/slide${++slideCountRef.current}.jpg)`;
-    }, 5500);
+    };
+  }, [slideState, slideCount]);
+
+  useEffect(() => {
+    for (let i = 0; i < slideListRef.current.children.length; i++) {
+      preloadRef.current.style.backgroundImage += `,url(images/slide${i +
+        1}.jpg)`;
+    }
+    slideListRef.current.children[slideCount].classList.add('running');
+    setSlideCount(1);
   }, []);
+
   return (
-    <div id="slideShowWrap">
+    <div id="slideShowWrap" ref={slideWrapRef}>
       <div
-        id="slideShow"
+        id="preloadImage"
+        ref={preloadRef}
         style={{
-          backgroundImage: 'url(images/slide1.jpg)',
+          backgroundImage: 'url()',
         }}
-        ref={slideRef}
       />
+      <a href="/">
+        <div
+          id="slideShow"
+          style={{
+            backgroundImage: 'url(images/slide1.jpg)',
+          }}
+          ref={slideRef}
+        />
+      </a>
       <div
         style={{
           width: '100%',
@@ -47,27 +111,27 @@ export default function SlideShow() {
         }}
       />
       <ul ref={slideListRef}>
-        <li className="running">
+        <li data-number="1">
           <div className="cover" />
           <p className="sub">최근 인기 프로그램</p>
           <p className="programName">최고다 호기심딱지</p>
         </li>
-        <li>
+        <li data-number="2">
           <div className="cover" />
           <p className="sub">최근 인기 프로그램</p>
-          <p className="programName">출동 슈펑윙스</p>
+          <p className="programName">출동 슈퍼윙스</p>
         </li>
-        <li>
+        <li data-number="3">
           <div className="cover" />
           <p className="sub">최근 인기 프로그램</p>
           <p className="programName">생방송 판다다</p>
         </li>
-        <li>
+        <li data-number="4">
           <div className="cover" />
           <p className="sub">최근 인기 프로그램</p>
           <p className="programName">딩동댕 유치원</p>
         </li>
-        <li>
+        <li data-number="5">
           <div className="cover" />
           <p className="sub">최근 인기 프로그램</p>
           <p className="programName">뽀로로와 노래해요</p>
